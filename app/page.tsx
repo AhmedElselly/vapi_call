fd2abde2273e0f528872349802d9c1d9395a722f
ahmed elselly <ahmedelselly87@gmail.com>
2026-02-21 12:11:51 +0200

    added padding to the home page

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import VapiWidget from "@/components/Vapi";
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
+    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black px-8">
       <HomeComponent />
     </div>
   );
